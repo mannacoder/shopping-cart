a4af3c8fec175a9ec480541929465408232fcda4
Vaibhav Kumar Singh <vaibhavv.singh11@gmail.com>
2023-03-07 19:09:06 +0530

    updated ui

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -14,10 +14,13 @@ const Auth = () => {
     <div className="container">
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="id">Id</label>
-        <input type="text" name="id" id="id" />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input placeholder="john@email.com" type="text" name="id" id="id" />
+        <input
+          placeholder="******"
+          type="password"
+          name="password"
+          id="password"
+        />
         <button className="login-btn" type="submit">
           Login
         </button>
